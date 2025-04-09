6ae7d7e0f995b05c6756391cdaa875a5adcb9314
Anna P <annapet200313@yandex.ru>
2025-04-09 21:12:41 +0800

    new shelter

--- shelter/menu.js
+++ shelter/menu.js
@@ -1,21 +1,85 @@
+
+/* Попытка слайдера*/ 
+
+const slider = document.querySelector('.conteiner__slider__img');
+const sliderImg = document.querySelectorAll('.slider__item');
+const sliderLine = document.querySelector('.slider__img');
+
+const sliderBtnLeft = document.querySelector('.slider__button-left')
+const sliderBtnRith = document.querySelector('.slider__button-right')
+
+let sliderCount = 0;
+let sliderWidth = slider.offsetWidth;
+
+sliderBtnRith.addEventListener('click',nextSlide);
+sliderBtnLeft.addEventListener('click',prevSlider)
+
+
+function nextSlide() {
+    sliderCount++;
+
+
+    if (sliderCount >= sliderImg.length) {
+        sliderCount = 0;
+    }
+
+    rollSlider()
+}
+
+
+function prevSlider() {
+    sliderCount--;
+
+    if (sliderCount < 0) {
+        sliderCount = sliderImg.length -1;
+    }
+
+    rollSlider()
+}
+
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`
+}
+
+
+
+
+/* Открытие и закрытие бургер-меню */
+
 const menu = document.querySelector('.navbar__body');
 const menuBrg = document.querySelector('.menu__button');
-
+const navbarBack = document.querySelector('.navbar__back');
 const body = document.body;
 
 if (menu && menuBrg) {
     menuBrg.addEventListener('click',() => {
         menu.classList.toggle('active')
+        
         menuBrg.classList.toggle('active')
         body.classList.toggle('lock')
     })
 }
 
 
+
+
+/*----------------------------------------- */ 
+
+/* Открытие и закрытие поп-ап карточки */ 
+ 
 const popButton = document.querySelector('.button__slider__pet');
 
 const popUp = document.querySelector('.pop__up__conteiner');
 
+const closePopUp = document.querySelector('.button__pop-up');
+
 popButton.addEventListener('click', () => {
     popUp.classList.toggle('hidden')
 })
+
+closePopUp.addEventListener('click', () =>{
+    popUp.classList.toggle('hidden')
+})
+
+/*-----------------------------------------*/
+
